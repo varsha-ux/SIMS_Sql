34d516e40f2dd5065b8cb4ef2e1651c33629cc20
varsha-ux <varsharoy841@gmail.com>
2026-08-13 14:45:11 +0530

    Table insert

--- MusafirCafe/main.sql
+++ MusafirCafe/main.sql
@@ -16,7 +16,7 @@ CREATE TABLE MenuItems (
     Price DECIMAL(10, 2) NOT NULL,
     PrepTimeMinutes INT NOT NULL
 )
-
+-- CUSTOMER TABLE --
 CREATE TABLE Customers (
     CustomerID INT IDENTITY(1,1) PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
@@ -24,3 +24,34 @@ CREATE TABLE Customers (
     Phone VARCHAR(20),
     CustomerType VARCHAR(20) NOT NULL CHECK (CustomerType IN ('Walk-in', 'App'))
 )
+
+CREATE TABLE Suppliers (
+    SupplierID INT IDENTITY(1,1) PRIMARY KEY,
+    SupplierName VARCHAR(100) NOT NULL,
+    ContactNumber VARCHAR(20),
+    Address VARCHAR(MAX)
+)
+
+-- DEPENDENCY TABLES --
+
+-- EMPLOYEE TABLE --
+CREATE TABLE Employees (
+    EmployeeID INT IDENTITY(1,1) PRIMARY KEY,
+    StoreID INT NOT NULL,
+    Name VARCHAR(100) NOT NULL,
+    Role VARCHAR(20) NOT NULL CHECK (Role IN ('Barista', 'Store Manager', 'Admin')),
+    Email VARCHAR(100),
+    Phone VARCHAR(20),
+    FOREIGN KEY (StoreID) REFERENCES Stores(StoreID)
+)
+
+CREATE TABLE Inventory (
+    IngredientID INT IDENTITY(1,1) PRIMARY KEY,
+    StoreID INT NOT NULL,
+    IngredientName VARCHAR(100) NOT NULL,
+    Unit VARCHAR(10) NOT NULL CHECK (Unit IN ('Kg', 'Litre', 'Piece', 'Pack')),
+    CurrentStock DECIMAL(10, 2) NOT NULL,
+    ReorderLevel DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (StoreID) REFERENCES Stores(StoreID)
+)
+
